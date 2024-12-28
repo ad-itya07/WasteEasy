8fbaa831ba41ad17d8860eeb70048f969179e6c0
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2024-12-28 14:55:08 +0530

    Db connection

---
+++ src/libs/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+  try {
+    if (mongoose.connection.readyState === 1) {
+      return;
+    }
+    
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw error;
+  }
+}
