576af6b94ab677f3367a28ec08a27fee4051de8e
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-30 21:21:50 +0530

    Image allowed-urls configured

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+// configured IMAGE url to accept different urls (but still not working)
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
